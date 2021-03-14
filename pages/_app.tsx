05700e3ac632f1f4594dbf1ce8f31856a5786056
marian2js <marianomdq1@gmail.com>
2021-03-14 18:27:19 +0100

    Set aws config on Auth module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Amplify from 'aws-amplify'
-import config from '../src/aws-exports'
+import Amplify, { Auth } from 'aws-amplify'
+import awsconfig from '../src/aws-exports'
 
 import 'antd/dist/antd.css'
 import '../styles/vars.css'
 import '../styles/global.css'
 import '../components/landing/Landing.css'
 
-Amplify.configure({
-  ...config,
-  ssr: true
-})
+// Amplify configuration
+const authConfig = { ...awsconfig, ssr: true }
+Amplify.configure(authConfig)
+Auth.configure(authConfig)
 
 export default function MyApp ({ Component, pageProps }): JSX.Element {
   return <Component {...pageProps} />
--- src/utils/user.utils.ts
+++ src/utils/user.utils.ts
@@ -2,9 +2,14 @@ import { withSSRContext } from 'aws-amplify'
 import { NextApiRequest } from 'next'
 
 export async function getAuthUsername (req: NextApiRequest): Promise<string | undefined> {
+  console.log('=== getAuthUsername ===')
   const { Auth } = withSSRContext({ req })
+  console.log('Auth =>', Auth)
   try {
     const user = await Auth.currentAuthenticatedUser()
+    console.log('User =>', user)
     return user.username
-  } catch (e) {}
+  } catch (e) {
+    console.log('Error =>', e)
+  }
 }
